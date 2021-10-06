75a7b1dc9ab3fad0827f78d709cd8fe62ee848b1
german <germikgerman@gmail.com>
2021-10-06 21:42:36 +0300

    swagger header added

--- src/main/java/ru/bugprod/webtable/controller/IOController.java
+++ src/main/java/ru/bugprod/webtable/controller/IOController.java
@@ -1,9 +1,12 @@
 package ru.bugprod.webtable.controller;
 
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ResponseHeader;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.fileupload.FileItemStream;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,11 +21,12 @@ import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class IOController {
 
     private final DataFrameRepository repository;
 
-    @ApiOperation(value = "Импортировать файл в формате csv.")
+    @ApiOperation(value = "Импортировать файл в формате csv.", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @PostMapping("/import")
     public ResponseEntity<String> handleUpload(@RequestHeader String sessionKey, HttpServletRequest request) throws Exception {
         var upload = new ServletFileUpload();
@@ -36,10 +40,13 @@ public class IOController {
         return ResponseEntity.ok("Ok!");
     }
 
-    @ApiOperation(value = "Экспортировать таблицу в формат csv.")
+    @ApiOperation(value = "Экспортировать таблицу в формат csv.",
+            produces = MediaType.TEXT_PLAIN_VALUE,
+            responseHeaders = {@ResponseHeader(name = "Content-Disposition", description = "attachment; filename=Название_датасета")})
     @GetMapping(value = "/export")
     public void getFile(@RequestHeader String sessionKey, HttpServletResponse response) throws Exception {
         var holder = repository.exportData(sessionKey);
+        response.setContentType(MediaType.TEXT_PLAIN_VALUE);
         response.setHeader("Content-Disposition", "attachment; filename=" + holder.getTableName());
         try (var stream = holder.openStream()) {
             org.apache.commons.io.IOUtils.copy(stream, response.getOutputStream());
