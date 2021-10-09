7a56aee8521b1904cf99f1102d93e903421ac6c0
Igor Borzunov <Igor.Borzunov@rosbank.ru>
2021-10-09 16:32:53 +0300

    Минорные правки стилей

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Layout } from "antd";
 import { BrowserRouter as Router } from "react-router-dom";
+import logo from './img/logo.png'
 
 import "antd/dist/antd.css";
 import "./App.css";
@@ -78,14 +79,17 @@ function App() {
     return (
         <Router>
             <Layout>
-                <Header style={{ padding: 0 }} />
-                <Content style={{ margin: "24px 16px 0" }}>
-                    <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                <Header style={{padding: 0, backgroundColor: '#EBF3FE' }}>
+                    <img src={logo} width={100} height={40} style={{padding: 5, marginLeft: 15 }} />
+                </Header>
+                <Content style={{margin: "24px 16px 0"}}>
+                    <div className="site-layout-background" style={{padding: 24, minHeight: 360 }}>
                         {/* <Table columns={columns} dataSource={data} onChange={onChange} /> */}
                         {/* <MainTable/> */}
                         <Graph />
                     </div>
                 </Content>
+                <Footer style={{textAlign: "center"}}>Багетный прод © 2021</Footer>
                 <Footer style={{ textAlign: "center" }}>© 2021</Footer>
             </Layout>
         </Router>
