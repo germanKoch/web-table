56d098a3bf2388b8203d02660ca3cd286321c6d1
german <germikgerman@gmail.com>
2021-10-09 14:45:36 +0300

    additional sample data added.

--- src/main/java/ru/bugprod/webtable/repository/mock/DatahubMock.java
+++ src/main/java/ru/bugprod/webtable/repository/mock/DatahubMock.java
@@ -54,6 +54,83 @@ public class DatahubMock {
                         new Field("deletion_reason", "string")
                 ), "record", "fct_users_deleted", "com.linkedin.dataset", "Sample users dataset"
         ));
+        data.add(new DatasetMetadata(
+                getList(
+                        new Field("name", "string"),
+                        new Field("price", "number"),
+                        new Field("code", "string"),
+                        new Struct(
+                                "market",
+                                "struct",
+                                getList(
+                                        new Field("name", "string"),
+                                        new Field("city", "string"),
+                                        new Field("branch_code", "string")
+                                )
+                        ),
+                        new Field("supply_date", "date")
+                ), "record", "ProductsSample", "com.linkedin.dataset", "ProductsSample dataset"
+        ));
+        data.add(new DatasetMetadata(
+                getList(
+                        new Field("level", "string"),
+                        new Struct(
+                                "coordinates",
+                                "struct",
+                                getList(
+                                        new Field("class", "string"),
+                                        new Field("string_number", "number"),
+                                        new Field("date", "date")
+                                )
+                        )
+                ), "record", "LogSample", "com.linkedin.dataset", "LogSample dataset"
+        ));
+        data.add(new DatasetMetadata(
+                getList(
+                        new Field("name", "string"),
+                        new Field("population", "number"),
+                        new Struct(
+                                "capital",
+                                "struct",
+                                getList(
+                                        new Field("name", "string"),
+                                        new Struct("coordinates", "struct",
+                                                getList(new Field("lat", "number"),
+                                                        new Field("lng", "number"))
+                                        )
+                                )
+                        )
+                ), "record", "CountriesDataset", "com.linkedin.dataset", "CountriesDataset dataset"
+        ));
+        data.add(new DatasetMetadata(
+                getList(
+                        new Field("name", "string"),
+                        new Field("average_height", "number"),
+                        new Field("age", "number")
+                ), "record", "GardenTreesDataset", "com.linkedin.dataset", "GardenTreesDataset dataset"
+        ));
+        data.add(new DatasetMetadata(
+                getList(
+                        new Field("name", "string"),
+                        new Field("name", "number"),
+                        new Struct(
+                                "students_info",
+                                "struct",
+                                getList(
+                                        new Field("students_num", "number"),
+                                        new Field("average_rate", "number")
+                                )
+                        ),
+                        new Struct(
+                                "teachers_info",
+                                "struct",
+                                getList(
+                                        new Field("teachers_num", "number"),
+                                        new Field("average_salary", "number")
+                                )
+                        )
+                ), "record", "UniversDataset", "com.linkedin.dataset", "UniversDataset dataset"
+        ));
     }
 
     private List<Field> getList(Field... fields) {
